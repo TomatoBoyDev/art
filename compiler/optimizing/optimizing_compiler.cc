2e72bd3162ef41810f33e5e4a201960f54654e96
Nicolas Geoffray <ngeoffray@google.com>
2019-08-07 10:41:09 +0100

    Don't compile OSR methods that have phi equivalents at loop entry
    We currently don't handle this in the stack map, where we only encode
    one stack slot for a dex register.
    
    Bug: 136698025
    Test: 721-osr
    Change-Id: Ib395ed1165387ad5446a463c307cc0a45e365885
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- compiler/optimizing/optimizing_compiler.cc
+++ compiler/optimizing/optimizing_compiler.cc
@@ -865,6 +865,11 @@ CodeGenerator* OptimizingCompiler::TryCompile(ArenaAllocator* allocator,
                           MethodCompilationStat::kNotCompiledIrreducibleLoopAndStringInit);
           break;
         }
+        case kAnalysisFailPhiEquivalentInOsr: {
+          MaybeRecordStat(compilation_stats_.get(),
+                          MethodCompilationStat::kNotCompiledPhiEquivalentInOsr);
+          break;
+        }
         case kAnalysisSuccess:
           UNREACHABLE();
       }
